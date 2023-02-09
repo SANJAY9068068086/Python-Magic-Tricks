eb50c9776cb6e88fd59660d48d5188f80a42d658
SANJAY PANCHAL <79782968+SANJAY9068068086@users.noreply.github.com>
2023-02-09 23:31:41 +0530

    Add files via upload

---
+++ Birthday Program Python.py
@@ -0,0 +1,15 @@
+# Auto Write Program
+
+import pyautogui as auto
+import time
+
+time.sleep(5)
+
+#a = print("\U0001F382")
+
+for i in range(1000):
+    auto.write("Happy Birthday Pandey Ji")
+    auto.press("enter")
+    
+
+    
